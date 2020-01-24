341c213bf1b0a686dc447a6521a284a32eca1dbb
Jaskanwal - 1 <jaskanwalmundra@gmail.com>
2020-01-23 18:04:50 -0800

    Thermistor drivers
    Added and updated declarations for reading thermistor values in different formats.

--- thermister_expansion/thermistor.h
+++ thermister_expansion/thermistor.h
@@ -8,15 +8,32 @@
 #ifndef THERMISTOR_H_
 #define THERMISTOR_H_
 
+#define MAX_TEMP_CHARGING 45
+#define MAX_TEMP_RUNNING  60
+
 #include "sys_common.h"
 
 
-/*Thermistor Interface Functions*/
+/*TERMISTOR INTERFACE FUNCTIONS***************************************************************************************************************/
+
+/*Setup*/
+
 void setup_thermistor ();               //prepare the thermistor to start reading
+
+/*Assessing usage status*/
+
 bool thermistor_measuring_charge ();    //inquire whether the thermistor is measuring while the car is charging?
 bool thermistor_measuring_run ();       //inquire whether the thermistor is measuring while the car is running?
+
+/*Reading Thermistor values*/
+
 uint16_t read_thermistor ();            //reads and returns the thermistor values
-uint16_t verify_fault_thermistor ();    //verifies if the temperatures are within acceptable limits
+
+/*Managing faults*/
+
+uint16_t read_fault_thermistor ();    //verifies if the temperatures are within acceptable limits
+
+
 bool flag_fault_thermistor ();          //flags a fault occurrence
 void send_fault_details_thermistor ();  //sends fault details to the VCU or the Dashboard or...... over SCI or something
 
