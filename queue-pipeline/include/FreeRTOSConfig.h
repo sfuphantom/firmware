3e5dbd954a11d16cb9a1f8582ce55057dc182ad1
rafguevara14 <66582348+rafguevara14@users.noreply.github.com>
2022-05-11 21:24:12 -0700

    configure het pin

--- queue-pipeline/include/FreeRTOSConfig.h
+++ queue-pipeline/include/FreeRTOSConfig.h
@@ -100,7 +100,7 @@
 #define configCPU_CLOCK_HZ			  ( ( unsigned portLONG ) 80000000 ) /* Timer clock. */
 #define configTICK_RATE_HZ			  ( ( TickType_t ) 1000 )
 #define configMAX_PRIORITIES		  ( 5 )
-#define configMINIMAL_STACK_SIZE	  ( ( unsigned portSHORT ) 1024 )
+#define configMINIMAL_STACK_SIZE	  ( ( unsigned portSHORT ) 128 )
 #define configTOTAL_HEAP_SIZE		  ( ( size_t ) 8192 )
 #define configMAX_TASK_NAME_LEN		  ( 16 )
 #define configIDLE_SHOULD_YIELD		  1
